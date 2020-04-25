754b8f5f97bd00ae1e906925c583eb9472fdef3c
codeavenger1 <shivakansagara@gmail.com>
2020-04-25 11:31:31 -0700

    idk

---
+++ src/section2/Houses.java
@@ -0,0 +1,22 @@
+package section2;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+
+	Robot r = new Robot();
+	
+
+public void run() {
+	r.setSpeed(10);
+	
+}
+public void drawHouse(String height, String color) {
+	Robot r = new Robot();
+	if (height.equals("small")) {
+		r.move(distance);
+	}
+	
+	
+}
+}
