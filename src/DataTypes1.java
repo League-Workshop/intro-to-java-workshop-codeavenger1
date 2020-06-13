fc3de5f43309b07b1456265f3a49e93e51306534
codeavenger1 <shivakansagara@gmail.com>
2020-06-13 11:36:10 -0700

    update

--- src/DataTypes1.java
+++ src/DataTypes1.java
@@ -2,27 +2,25 @@ import javax.swing.JOptionPane;
 
 public class DataTypes1 {
 //	Write a program that asks the user a question. 
-//	If their answer is correct, tell them it's correct and increase their score by one.
+//	If their answer is correct, increase their score by one.
 //	Tell the user their score at the end.
 
 	// String tells you that it stores words
 	// int tells you that it stores numbers
 
 	public static void main(String[] args) {
-
 		int score = 0;
-
-		String colors = JOptionPane.showInputDialog("What do you call an animal that only eats plants?");
-		if (colors.equals("an herbivore")) {
+		
+		String poem = JOptionPane.showInputDialog("Who wrote Romeo and Juliet?");
+		if (poem.equals("Shakespeare")) {
+			score+=1;
+		}
+		
+		String season = JOptionPane.showInputDialog("What season comes before summer?");
+		if (season.equals("spring")) {
 			score++;
 		}
-
-		String spring = JOptionPane.showInputDialog("What season comes before summer?");
-		if (spring.equals("spring")) {
-			score++;
-		}
-
+		
 		JOptionPane.showMessageDialog(null, "Your final score is " + score);
-
 	}
 }
--- src/DataTypes2.java
+++ src/DataTypes2.java
@@ -7,31 +7,8 @@ public class DataTypes2 {
 
 		// Boolean tells you that it stores true or false
 
-		boolean isGirl = false;
-		boolean hasBrownEyes = false;
+		boolean isGirl;
+		boolean hasBrownEyes;
 
-		String girl = JOptionPane.showInputDialog("Are you a girl?");
-		if (girl.equals("yes")) {
-			isGirl = true;
-		} else if (girl.equals("no")) {
-			isGirl = false;
-		}
-
-		String eyes = JOptionPane.showInputDialog("Do you have brown eyes?");
-		if (eyes.equals("yes")) {
-			hasBrownEyes = true;
-		} else if (eyes.equals("no")) {
-			hasBrownEyes = false;
-		}
-
-		if (isGirl == true && hasBrownEyes == true) {
-			JOptionPane.showMessageDialog(null, "You are a girl and have brown eyes.");
-		} else if (isGirl == true && hasBrownEyes == false) {
-			JOptionPane.showMessageDialog(null, "You are a girl and do not have brown eyes.");
-		} else if (isGirl == false && hasBrownEyes == true) {
-			JOptionPane.showMessageDialog(null, "You are not a girl and have brown eyes.");
-		} else {
-			JOptionPane.showMessageDialog(null, "You are not a girl and do not have brown eyes.");
-		}
 	}
 }
